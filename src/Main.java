2d57fd2e316ced63b30e81bc6718780fe373c5b5
Asliddin <asliddinbokijonov@gmail.com>
2025-02-09 18:51:07 -0500

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        int price = 10;
+        double taxRate = 0.05;
+        double tax = price * taxRate;
+        double total = price + tax;
+        System.out.println("The total price is " + total);
+    }
+
+}
